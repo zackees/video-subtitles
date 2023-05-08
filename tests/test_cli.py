42d4977c3b79924b41ecb6ad04e5bb7fc8660a63
zackees <z@zackees.com>
2023-05-08 15:55:55 -0700

    try and fix github runner test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,10 +9,12 @@ os.chdir(HERE)
 
 COMMAND = "videosubtitles video.mp4 --api-key free --languages es,fr,zh --model large"
 
+IS_GITHUB = os.environ.get("GITHUB_ACTIONS", False)
 
 class MainTester(unittest.TestCase):
     """Main tester class."""
 
+    @unittest.skipIf(IS_GITHUB, "Skip if running on GitHub Actions.")
     def test_imports(self) -> None:
         """Test command line interface (CLI)."""
         rtn = os.system(COMMAND)
